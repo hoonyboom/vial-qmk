479620c46fc9694250e30f2aadded309e643730a
hoonyboom <hyezoprk@kakao.com>
2024-11-29 20:13:08 +0900

    리더키 수정

--- users/hyezo/hyezo.c
+++ users/hyezo/hyezo.c
@@ -42,41 +42,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // 리더키
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_F)) {
-        SEND_STRING("QMK is awesome.\n");
-    } else if (leader_sequence_two_keys(KC_G, KC_H)) {
-        tap_code16(LSA(KC_1));
-    } else if (leader_sequence_two_keys(KC_G, KC_J)) {
-        tap_code16(LSA(KC_2));
-    } else if (leader_sequence_two_keys(KC_G, KC_K)) {
-        tap_code16(LSA(KC_3));
-    } else if (leader_sequence_two_keys(KC_G, KC_L)) {
-        tap_code16(LSA(KC_4));
-    } else if (leader_sequence_two_keys(KC_G, KC_O)) {
-        tap_code16(LSA(KC_O));
-    } else if (leader_sequence_two_keys(KC_G, KC_F)) {
-        tap_code16(LSA(KC_F));
-    } else if (leader_sequence_two_keys(KC_G, KC_I)) {
-        tap_code16(LSA(KC_I));
-    } else if (leader_sequence_two_keys(KC_G, KC_S)) {
-        tap_code16(LSA(KC_5));
-    } else if (leader_sequence_two_keys(KC_G, KC_X)) {
-        tap_code16(LSA(KC_X));
-    } else if (leader_sequence_two_keys(KC_G, KC_Y)) {
-        tap_code16(LSA(KC_Y));
-    } else if (leader_sequence_two_keys(KC_G, KC_B)) {
-        tap_code16(LSA(KC_B));
-    } else if (leader_sequence_two_keys(KC_G, KC_1)) {
-        tap_code16(LSA(KC_7));
-    } else if (leader_sequence_two_keys(KC_G, KC_2)) {
-        tap_code16(LSA(KC_8));
-    } else if (leader_sequence_two_keys(KC_G, KC_3)) {
-        tap_code16(LSA(KC_9));
-    } else if (leader_sequence_two_keys(KC_G, KC_4)) {
-        tap_code16(LSA(KC_0));
-    } else if (leader_sequence_two_keys(KC_G, KC_N)) {
-        tap_code16(LSA(KC_N));
-    } else if (leader_sequence_two_keys(KC_G, KC_P)) {
-        tap_code16(LSA(KC_P));
+        SEND_STRING(" =>() ");
+    } else if (leader_sequence_one_key(KC_C)) {
+        SEND_STRING("console.log()"SS_TAP(X_LEFT));
     } else if (leader_sequence_two_keys(KC_G, KC_Q)) {
         tap_code16(LSA(KC_Q));
     } else if (leader_sequence_two_keys(KC_G, KC_W)) {
@@ -85,13 +53,43 @@ void leader_end_user(void) {
         tap_code16(LSA(KC_E));
     } else if (leader_sequence_two_keys(KC_G, KC_R)) {
         tap_code16(LSA(KC_R));
-    } else if (leader_sequence_two_keys(KC_A, KC_A)) {
-        SEND_STRING("hoonyboom@gmail.com");
-    } else if (leader_sequence_two_keys(KC_A, KC_S)) {
-        SEND_STRING("01073510097");
-    } else if (leader_sequence_two_keys(KC_A, KC_U)) {
-        SEND_STRING("P911160088896");
-    } else if (leader_sequence_two_keys(KC_A, KC_B)) {
-        SEND_STRING("911029");
+    } else if (leader_sequence_two_keys(KC_G, KC_U)) {
+        tap_code16(LSA(KC_U));
+    } else if (leader_sequence_two_keys(KC_G, KC_I)) {
+        tap_code16(LSA(KC_I));
+    } else if (leader_sequence_two_keys(KC_G, KC_O)) {
+        tap_code16(LSA(KC_O));
+    } else if (leader_sequence_two_keys(KC_G, KC_P)) {
+        tap_code16(LSA(KC_P));
+    } else if (leader_sequence_two_keys(KC_G, KC_A)) {
+        tap_code16(LSA(KC_A));
+    } else if (leader_sequence_two_keys(KC_G, KC_S)) {
+        tap_code16(LSA(KC_S));
+    } else if (leader_sequence_two_keys(KC_G, KC_D)) {
+        tap_code16(LSA(KC_D));
+    } else if (leader_sequence_two_keys(KC_G, KC_F)) {
+        tap_code16(LSA(KC_F));
+    } else if (leader_sequence_two_keys(KC_G, KC_H)) {
+        tap_code16(LSA(KC_H));
+    } else if (leader_sequence_two_keys(KC_G, KC_J)) {
+        tap_code16(LSA(KC_J));
+    } else if (leader_sequence_two_keys(KC_G, KC_K)) {
+        tap_code16(LSA(KC_K));
+    } else if (leader_sequence_two_keys(KC_G, KC_L)) {
+        tap_code16(LSA(KC_L));
+    } else if (leader_sequence_two_keys(KC_G, KC_Z)) {
+        tap_code16(LSA(KC_Z));
+    } else if (leader_sequence_two_keys(KC_G, KC_X)) {
+        tap_code16(LSA(KC_X));
+    } else if (leader_sequence_two_keys(KC_G, KC_C)) {
+        tap_code16(LSA(KC_C));
+    } else if (leader_sequence_two_keys(KC_G, KC_V)) {
+        tap_code16(LSA(KC_V));
+    } else if (leader_sequence_two_keys(KC_G, KC_B)) {
+        tap_code16(LSA(KC_B));
+    } else if (leader_sequence_two_keys(KC_G, KC_N)) {
+        tap_code16(LSA(KC_N));
+    } else if (leader_sequence_two_keys(KC_G, KC_M)) {
+        tap_code16(LSA(KC_M));
     }
 }
